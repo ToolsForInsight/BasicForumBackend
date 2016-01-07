620ab9f0c376ccf23d99938885ea383065c4211e
Ryan Connor <ryanwc13@gmail.com>
2016-01-07 16:26:12 +0700

    post and get all posts from the database

--- forumdb.py
+++ forumdb.py
@@ -1,11 +1,8 @@
 #
 # Database access functions for the web forum.
 # 
-
+import psycopg2
 import time
-
-## Database connection
-DB = []
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +13,14 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    ## Database connection
+    DB = psycopg2.connect("dbname=forum")
+
+    cursor = DB.cursor()
+    cursor.execute("select time, content from posts order by time desc;")
+    posts = ({'content': str(row[1]), 'time': str(row[0])}
+             for row in cursor.fetchall())
+    DB.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +30,9 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    ## Database connection
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
+    cursor.execute("insert into posts (content) values (%s)", (content,))
+    DB.commit()
+    DB.close()
